0a55cc5481cc2cc4b9d79155da96bc921006f7e9
GurinaArina0307 <gurinaarina227@gmail.com>
2025-03-13 15:56:31 +0500

    Add files via upload

---
+++ pr_9_2_Gurina/src/Cashier.kt
@@ -0,0 +1,3 @@
+class Cashier (val name: String){
+
+}
---
+++ pr_9_2_Gurina/src/Zayavka.kt
@@ -0,0 +1,7 @@
+import javax.print.attribute.standard.Destination
+
+class Zayavka (
+    val passager: Passager,
+    val destination: Destination,
+    val dataTime: LocalDateTime
+        )
